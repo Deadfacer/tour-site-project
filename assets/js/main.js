28f0c136a7ffc7c296ebb806ab83740ca13e1bb2
JEhrenworth <JEhrenworth18@avenues.org>
2015-11-09 21:35:15 -0500

    Added nav-bar + about-us

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,43 @@
+// ####### Navbar #######
+
+// Sticky Header
+$(window).scroll(function() {
+	if ($(window).scrollTop() > $('.btn').offset().top) {
+		$('.custom-navbar').addClass('sticky');
+	} else {
+		$('.custom-navbar').removeClass('sticky');
+	}
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+	if ($('.custom-navbar').hasClass('open-nav')) {
+		$('.custom-navbar').removeClass('open-nav');
+	} else {
+		$('.custom-navbar').addClass('open-nav');
+	}
+});
+
+$('.custom-navbar li a').click(function() {
+	if ($('.custom-navbar').hasClass('open-nav')) {
+		$('.navigation').removeClass('open-nav');
+		$('.custom-navbar').removeClass('open-nav');
+	}
+});
+
+// Navigation Scroll
+$('nav a').click(function(event) {
+	var id = $(this).attr("href");
+	var offset = 70;
+	var target = $(id).offset().top - offset;
+	$('html, body').animate({
+		scrollTop: target
+	}, 500);
+	event.preventDefault();
+});
+
+// ####### Image Resize #######
+
 var OrigWidth = $("#img1").width();
 
 $(window).on('resize', function() {
@@ -9,18 +49,18 @@ $(window).on('resize', function() {
 	}
 }).trigger('resize');
 
-function fitImages() { //there is duplicate code but we are only fitting 4 images
-	//so it isn't worth it to follow DRY principles
+function fitImages() { // there is duplicate code but we are only fitting 4 images
+	// so it isn't worth it to follow DRY principles
 
-	//images 1-2
+	// images 1-2
 	$("#img1").removeAttr("style");
 	$("#img2").removeAttr("style");
 	var changedWidth = $("#img1").height();
 	var h1 = $("#img1").height();
 	var h2 = $("#img2").height();
-	//it can be done only with height but using double check ratio of the images is a bit more acurate
+	// it can be done only with height but using double check ratio of the images is a bit more acurate
 	if (changedWidth < OrigWidth) { //expand
-		//using higher image as refference when maximize
+		// using higher image as refference when maximize
 		if (h1 > h2) {
 			$("#img2").height(h1);
 		}
@@ -29,7 +69,7 @@ function fitImages() { //there is duplicate code but we are only fitting 4 image
 		}
 	}
 	else {
-		//using lower image as refference when minimize
+		// using lower image as refference when minimize
 		if (h1 < h2) {
 			$("#img1").height(h2);
 		}
@@ -39,7 +79,7 @@ function fitImages() { //there is duplicate code but we are only fitting 4 image
 	}
 	OrigWidth = changedWidth;
 
-	//images 3-4
+	// images 3-4
 	$("#img3").removeAttr("style");
 	$("#img4").removeAttr("style");
 	var changedWidth = $("#img3").height();
